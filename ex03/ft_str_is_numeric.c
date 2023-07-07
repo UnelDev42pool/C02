16f283516c72ce5d8134072a54ee9975260b3c49
Enee Di Iorio <edi-iori@z3r11p5.42lyon.fr>
2023-07-07 18:20:16 +0200

    add ft_str_is_number

---
+++ ex03/ft_str_is_numeric.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_numeric.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edi-iori <edi-iori@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/07 14:24:36 by edi-iori          #+#    #+#             */
+/*   Updated: 2023/07/07 18:19:28 by edi-iori         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+int	char_is_numeric(char c)
+{
+	if (c <= '0' && c >= '1')
+	{
+		return (1);
+	}
+	return (0);
+}
+
+int	ft_str_is_numeric(char *str)
+{
+	int	i;
+
+	if (str[0] == '\0')
+	{
+		return (1);
+	}
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (!char_is_numeric(str[i]))
+		{
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+/*int main(void)
+{
+	char source[] ="123";
+	char *point = source;
+	printf("%d\n", ft_str_is_numeric(point));
+	return 0;
+}*/
