39b23cb5e542d3a1c2a5fc4b31819bb3921b605a
Enee Di Iorio <edi-iori@z3r11p5.42lyon.fr>
2023-07-07 22:51:14 +0200

    add ex08

--- ex08/ft_strlowcase.c
+++ ex08/ft_strlowcase.c
@@ -1,35 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strupcase.c                                     :+:      :+:    :+:   */
+/*   ft_strlowcase.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: edi-iori <edi-iori@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/07 22:34:47 by edi-iori          #+#    #+#             */
-/*   Updated: 2023/07/07 22:51:02 by edi-iori         ###   ########lyon.fr   */
+/*   Created: 2023/07/07 22:48:12 by edi-iori          #+#    #+#             */
+/*   Updated: 2023/07/07 22:50:50 by edi-iori         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 // #include <stdio.h>
-int	char_is_alpha_low(char c)
+int	char_is_alpha_up(char c)
 {
-	if (c <= 'z' && c >= 'a')
+	if (c <= 'Z' && c >= 'A')
 	{
 		return (1);
 	}
 	return (0);
 }
 
-char	*ft_strupcase(char *str)
+char	*ft_strlowcase(char *str)
 {
 	int	last_char;
 
 	last_char = 0;
 	while (str[last_char] != '\0')
 	{
-		if (char_is_alpha_low(str[last_char]))
+		if (char_is_alpha_up(str[last_char]))
 		{	
-			str[last_char] = str[last_char] - 32;
+			str[last_char] = str[last_char] + 32;
 		}
 		last_char++;
 	}
@@ -37,8 +37,8 @@ char	*ft_strupcase(char *str)
 }
 // int main(void)
 // {
-// 	char source[] ="dfD";
+// 	char source[] ="dfDF";
 // 	char *point = source;
-// 	printf("%s\n", ft_strupcase(point));
+// 	printf("%s\n", ft_strlowcase(point));
 // 	return 0;
 // }
