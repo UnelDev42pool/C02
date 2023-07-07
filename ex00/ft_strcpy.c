f91e5e0f7da6dbd1cc16b99020e0c408a68708c5
Enee Di Iorio <edi-iori@z3r11p5.42lyon.fr>
2023-07-07 10:49:44 +0200

    add ft_strcpy

---
+++ ex00/ft_strcpy.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edi-iori <edi-iori@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/07 10:14:20 by edi-iori          #+#    #+#             */
+/*   Updated: 2023/07/07 10:49:28 by edi-iori         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include <stdio.h>
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
+
+// int main(void)
+// {
+//     char source[] = "hello world";
+// 	char *point = source;
+//     char destination[] = "                ";
+// 	char *pointeur = destination;
+//  	ft_strcpy(pointeur, point);
+//     printf("%s\n", pointeur);
+//     return 0;
+// }
