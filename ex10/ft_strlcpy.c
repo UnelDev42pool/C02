5ade77d284803ecf4a26a86ff74b201e8b11aa53
Enee Di Iorio <edi-iori@z3r11p5.42lyon.fr>
2023-07-08 11:12:18 +0200

    add ex10

---
+++ ex10/ft_strlcpy.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edi-iori <edi-iori@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/08 09:40:15 by edi-iori          #+#    #+#             */
+/*   Updated: 2023/07/08 10:59:07 by edi-iori         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include <stdio.h>
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		if(i < size)
+		{
+			dest[i] = src[i];
+		}
+		if ( i == size - 1)
+		{
+			dest[i] = '\0';
+		}
+		i++;
+	}
+	return (i);
+}
+// int	main()
+// {
+//  	char source[] ="hello world";
+//  	char dest[20]= "     ";
+// 	char *destpint = dest;
+//  	char *point = source;
+//  	printf("%d\n", ft_strlcpy(destpint, point, 11));
+// 	printf("%s\n", dest);
+//  	return 0;
+// }
